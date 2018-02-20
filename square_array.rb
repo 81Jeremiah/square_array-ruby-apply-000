c3c0f6a61b69a8667ab40a5aa4b64708979eca9f
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-02-20 16:50:45 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   #square each # in array
   square_num = []
-  array.each { |num| square_num << num ** 2}
+  array.each { |num| square_num.push num ** 2}
    square_num
  end
 def square_array_collect(array)
